f7be01e3be79ab384b667a74eb3f1cf229037678
wwf <865475679@qq.com>
2020-05-12 19:54:43 +0800

    主页布局及侧边栏获取数据

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,95 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主题区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#333644" text-color="#fff" active-text-color="#ffd04b">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
+              <!-- 二级菜单模板区域 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容主题 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menuList: []
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
   methods: {
     logout() {
       window.sessionStorage.clear();
       this.$router.push("./login");
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menuList = res.data;
+      console.log(res);
     }
   }
 };
 </script>
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373c40;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #ffffff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 20px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333644;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
