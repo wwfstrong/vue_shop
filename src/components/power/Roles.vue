910bccd45b6931d9b00271c0153d03d6b263ddca
wwf <865475679@qq.com>
2020-05-13 22:29:43 +0800

    权限列表，角色列表样式完成

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+      <!-- 添加用户区域 -->
+      <el-button type="primary">添加用户</el-button>
+      <!-- 权限列表区域 -->
+      <el-table :data="rolesList" border stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand"></el-table-column>
+        <!-- 索引列 -->
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //角色列表
+      rolesList: []
+    };
+  },
+  created() {
+    this.getRolesList();
+  },
+  methods: {
+    async getRolesList() {
+      const { data: res } = await this.$http.get("roles");
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取角色列表失败！");
+      }
+      return (this.rolesList = res.data);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
