dfb7389a5cdb22acbe16b471616e1b4c22c3d96a
wwf <865475679@qq.com>
2020-05-13 13:04:59 +0800

    用户列表渲染完成

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -16,14 +16,88 @@
           </el-input>
         </el-col>
         <el-col :span="4">
-            <el-button type="primary">添加用户</el-button>
+          <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+      <!-- 用户列表区域 -->
+      <el-table :data="userList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180">
+          <template>
+            <!-- 修改按钮 -->
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <!-- 删除按钮 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <!-- 分配角色按钮 -->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      //获取用户列表的参数对象
+      queryInfo: {
+        query: "",
+        //当前页数
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total: 0
+    };
+  },
+  created() {
+    this.getUserList();
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get("users", {
+        params: this.queryInfo
+      });
+      if (res.meta.status !== 200)
+        return this.$message.error("获取用户列表失败！");
+      this.userList = res.data.users;
+      this.total = res.data.total;
+      console.log(res);
+    },
+    // 监听 pagesize 改变事件
+    handleSizeChange(newSize){
+        this.queryInfo.pagesize = newSize
+        this.getUserList()
+    },
+    // 监听 页码值 改变事件
+    handleCurrentChange(newPage){
+        this.queryInfo.pagenum = newPage
+        this.getUserList()
+    }
+  }
+};
 </script>
 <style lang="less" scoped>
 </style>
