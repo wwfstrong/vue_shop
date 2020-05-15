0653223fec910baa4bc520a8d7e9a024b1ea8ad8
wwf <865475679@qq.com>
2020-05-15 16:02:43 +0800

    排序，操作UI完成

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -24,6 +24,7 @@
         border
         :show-row-hover="false"
       >
+        <!-- 是否有效 -->
         <template slot="isok" slot-scope="scope">
           <i
             class="el-icon-success"
@@ -31,6 +32,17 @@
             style="color:lightgreen"
           ></i>
           <i class="el-icon-error" v-else style="color:lightgreen"></i>
+        </template>
+        <!-- 排序 -->
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level ===0">一级</el-tag>
+          <el-tag type="success" size="mini" v-else-if="scope.row.cat_level ===1">二级</el-tag>
+          <el-tag type="warning" size="mini" v-else>三级</el-tag>
+        </template>
+        <!-- 操作 -->
+        <template slot="opt" slot-scope>
+          <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
         </template>
       </tree-table>
       <!-- 分页区域 -->
@@ -59,6 +71,20 @@ export default {
           type: "template",
           //表示当前这一列使用模板名称
           template: "isok"
+        },
+        {
+          label: "排序",
+          //表示将当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用模板名称
+          template: "order"
+        },
+        {
+          label: "操作",
+          //表示将当前列定义为模板列
+          type: "template",
+          //表示当前这一列使用模板名称
+          template: "opt"
         }
       ]
     };
