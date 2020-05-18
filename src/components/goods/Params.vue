cbf698bd5cbe72b7823b334ab7a803ea912f9bf6
wwf <865475679@qq.com>
2020-05-18 21:50:14 +0800

    分类参数添加功能实现

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -39,7 +39,29 @@
           <!-- 动态参数表格 -->
           <el-table :data="manyTableData" border stripe>
             <!-- 展开行 -->
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+              <template slot-scope="scope">
+                <!-- 循环渲染Tag标签 -->
+                <el-tag v-for="(item,i) in scope.row.attr_vals" :key="i" closable>{{item}}</el-tag>
+                <!-- 输入文本框 -->
+                <el-input
+                  class="input-new-tag"
+                  v-if="scope.row.inputVisible"
+                  v-model="scope.row.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(scope.row)"
+                  @blur="handleInputConfirm(scope.row)"
+                ></el-input>
+                <!-- 添加按钮 -->
+                <el-button
+                  v-else
+                  class="button-new-tag"
+                  size="small"
+                  @click="showInput(scope.row)"
+                >+ New Tag</el-button>
+              </template>
+            </el-table-column>
             <!-- 索引列 -->
             <el-table-column type="index" label="序号"></el-table-column>
             <el-table-column label="参数名称" prop="attr_name"></el-table-column>
@@ -210,6 +232,14 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error("获取参数列表失败！");
       }
+      res.data.forEach(item => {
+        item.attr_vals = item.attr_vals ? item.attr_vals.split(",") : [];
+        // 控制文本框的显示与隐藏
+        item.inputVisible = false;
+        // 文本框中输入的值
+        item.inputValue = "";
+      });
+      console.log(res.data);
       if (this.activeName === "many") {
         this.manyTableData = res.data;
       } else {
@@ -303,6 +333,39 @@ export default {
       this.$message.success("删除参数成功");
       //重新获取用户列表数据
       this.getParamsData();
+    },
+    // 文本框失去焦点，或按下Enter都会触发
+    async handleInputConfirm(row) {
+      if (row.inputValue.trim().length === 0) {
+        row.inputValue = "";
+        row.inputVisible = false;
+        return;
+      }
+      //如果没有return ，则证明输入的内容，需要做后续处理
+      row.attr_vals.push(row.inputValue.trim());
+      row.inputValue = "";
+      row.inputVisible = false;
+      //需要发起请求保存操作
+      const { data: res } = await this.$http.put(
+        `categories/${this.cateId}/attributes/${row.attr_id}`,
+        {
+          attr_name: row.attr_name,
+          attr_sel: row.attr_sel,
+          attr_vals: row.attr_vals.join(",")
+        }
+      );
+      if (res.meta.status !== 200) {
+        return this.$message.error("修改参数项失败！");
+      }
+      this.$message.success("修改参数项成功！");
+    },
+    // 点击按钮，展示文本输入框
+    showInput(row) {
+      row.inputVisible = true;
+      // 让文本框自动获得焦点
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
     }
   },
   computed: {
@@ -332,5 +395,11 @@ export default {
 <style lang="less" scoped>
 .cat_opt {
   margin: 15px 0;
+}
+.el-tag {
+  margin: 10px;
+}
+.input-new-tag {
+  width: 120px;
 }
 </style>
