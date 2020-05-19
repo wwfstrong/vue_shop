bfac1ea903e7a5f8b345925cbf2aebe342b02eae
wwf <865475679@qq.com>
2020-05-19 15:52:40 +0800

    添加商品的商品属性页面完成

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -64,7 +64,12 @@
               </el-checkbox-group>
             </el-form-item>
           </el-tab-pane>
-          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">
+            <!-- 渲染表单的Item项 -->
+            <el-form-item :label="item.attr_name" v-for="item in onlyTableData" :key="item.attr_id">
+              <el-input v-model="item.attr_vals"></el-input>
+            </el-form-item>
+          </el-tab-pane>
           <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
           <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
         </el-tabs>
@@ -111,7 +116,9 @@ export default {
         children: "children"
       },
       //   动态参数列表数据
-      manyTableData: []
+      manyTableData: [],
+      //   静态属性列表数据
+      onlyTableData: []
     };
   },
   created() {
@@ -158,6 +165,23 @@ export default {
             item.attr_vals.length === 0 ? [] : item.attr_vals.split(",");
         });
         this.manyTableData = res.data;
+      }
+      // 证明访问的是静态属性面板
+      if (this.activeIndex === "2") {
+        const { data: res } = await this.$http.get(
+          `categories/${this.cateId}/attributes`,
+          {
+            params: { sel: "only" }
+          }
+        );
+        if (res.meta.status !== 200) {
+          return thsi.$message.error("获取动态参数列表失败！");
+        }
+        res.data.forEach(item => {
+          item.attr_vals =
+            item.attr_vals.length === 0 ? [] : item.attr_vals.split(",");
+        });
+        this.onlyTableData = res.data;
       }
     }
   },
